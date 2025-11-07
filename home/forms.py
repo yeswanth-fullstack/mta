81571952b63e2a4c5a915352d8044093b54c1630
yeswanth-fullstack <yeswanth.fullstack@gmail.com>
2025-11-07 17:03:37 +0530

    dummy commit

--- home/forms.py
+++ home/forms.py
@@ -3,7 +3,7 @@ from django import forms
 
 class PersonalInformation(forms.Form):
     title = forms.ChoiceField(
-        choices=[("mr", "Mr."), ("ms", "Ms."), ("mrs", "Mrs.")], label="Title"
+        choices=[("mr", "Mr."), ("ms", "Ms."), ("mrs", "Mrs.")], label="Title "
     )
     first_name = forms.CharField(max_length=30, label="First Name")
     last_name = forms.CharField(max_length=30, label="Last Name")
